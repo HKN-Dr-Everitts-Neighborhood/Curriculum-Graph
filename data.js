c20cab06436545d3be56b3d709f9281830d0c707
Esteban Gomez Saavedra <gomezsa2@evrt-252-32.ews.illinois.edu>
2013-01-28 21:33:16 -0600

    Added the courses from ECE 424 to ECE 460. The same issue with non-existent prereqs still remains.

--- data.js
+++ data.js
@@ -326,5 +326,223 @@ var json = [
     "link": "https://wiki.engr.illinois.edu/pages/viewpage.action?pageId=213747887", 
     "prereqs": [ ["ECE 391"], ["CS 241"]],
     "coreqs": [ [] ]
+  },
+ {
+    "name": "ECE 424",
+    "title": "Computer Security II",
+    "crosslist": ["CS 463"],
+    "link": "https://wiki.engr.illinois.edu/pages/viewpage.action?pageId=213747887", 
+    "prereqs": [ ["ECE 422"], ["CS 461"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 425",
+    "title": "Intro to VLSI System Design",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+425+-+Intro+to+VLSI+System+Design", 
+    "prereqs": [ ["ECE 385", "ECE 411"], ["CS 232"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 428",
+    "title": "Distributed Systems",
+    "crosslist": ["CS 425"],
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/CS+425+%28ECE+428%29+-+Distributed+Systems", 
+    "prereqs": [ ["ECE 391"], ["CS 232"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 431",
+    "title": "Electric Machinery",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+431+-+Electric+Machinery", 
+    "prereqs": [ ["ECE 330"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 432",
+    "title": "Advanced Electric Machinery",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+431+-+Electric+Machinery", 
+    "prereqs": [ ["ECE 431"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 435",
+    "title": "Computer Networking Laboratory",
+    "crosslist": ["CS 436"],
+    "prereqs": [ ["CS 438"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 437",
+    "title": "Sensors and Instrumentation",
+    "link": "https://wiki.engr.illinois.edu/pages/viewpage.action?pageId=194289962", 
+    "prereqs": [ ["ECE 329"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 438",
+    "title": "Communication Networks",
+    "crosslist": ["CS 438"],
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/CS+438+-+Communication+Networks", 
+    "prereqs": [ ["ECE 391", "ECE 313"],
+                 ["ECE 391", "MATH 461"],
+                 ["ECE 391", "MATH 463"],
+                 ["CS 241", "ECE 313"],
+                 ["CS 241", "MATH 461"],
+                 ["CS 241", "MATH 463"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 439",
+    "title": "Wireless Networks",
+    "crosslist": ["CS 439"],
+    "prereqs": [ ["ECE 391", "ECE 313"],
+                 ["ECE 391", "MATH 461"],
+                 ["ECE 391", "MATH 463"],
+                 ["CS 241", "ECE 313"],
+                 ["CS 241", "MATH 461"],
+                 ["CS 241", "MATH 463"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 441",
+    "title": "Physcs & Modeling Semicond Dev",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+441+-+Physics+and+Modeling+of+Semiconductor+Devices", 
+    "prereqs": [ ["ECE 340"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 441",
+    "title": "Physcs & Modeling Semicond Dev",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+441+-+Physics+and+Modeling+of+Semiconductor+Devices", 
+    "prereqs": [ ["ECE 340"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 444",
+    "title": "Theory and Fabrication of Integrated Circuits",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+444+-+Theory+and+Fabrication+of+Integrated+Circuits", 
+    "prereqs": [ ["ECE 340"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 445",
+    "title": "Senior Design",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+445+-+Senior+Design", 
+    "prereqs": [ ["Senior Standing"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 447",
+    "title": "Active Microwave Ckt Design",
+    "prereqs": [ ["ECE 350", "ECE 353"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 448",
+    "title": "Artificial Intelligence",
+    "crosslist": ["CS 440"],
+    "prereqs": [ ["ECE 391"], ["CS 225"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 451",
+    "title": "Adv Microwave Measurements",
+    "prereqs": [ ["ECE 350"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 452",
+    "title": "Electromagnetic Fields and Electro-Optics",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+452+-+Electromagnetic+Fields+and+Electro-Optics", 
+    "prereqs": [ ["ECE 350"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 453",
+    "title": "Wireless Communication Systems",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+453+-+Wireless+Communications+Systems", 
+    "prereqs": [ ["ECE 329"]],
+    "coreqs": [ ["ECE 342"] ]
+  },
+  {
+    "name": "ECE 454",
+    "title": "Antennas",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+454+-+Antennas", 
+    "prereqs": [ ["ECE 350"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 455",
+    "title": "Optical Electronics",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+455+-+Optical+Electronics", 
+    "prereqs": [ ["ECE 350"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 456",
+    "title": "Global Nav Satellite Systems",
+    "prereqs": [ ["ECE 329", "ECE 310"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 457",
+    "title": "Microwave Devices & Circuits",
+    "link": "https://wiki.engr.illinois.edu/pages/viewpage.action?pageId=200478916", 
+    "prereqs": [ ["ECE 350"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 458",
+    "title": "Applic of Radio Wave Propag",
+    "prereqs": [ ["ECE 350"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 459",
+    "title": "Communications Systems",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+459+-+Communication+Systems", 
+    "prereqs": [ ["ECE 313"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "ECE 460",
+    "title": "Optical Imaging",
+    "prereqs": [ ["ECE 329"]],
+    "coreqs": [ ["ECE 313"] ]
+  },
+  {
+    "name": "CS 173",
+    "title": "Discrete Structures",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/CS+173+-+Discrete+Structures",
+    "prereqs": [ []],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "CS 125",
+    "title": "Intro to Computer Science",
+    "prereqs": [ []],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "CS 225",
+    "title": "Data Structures",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/CS+225+-+Data+Structures",
+    "prereqs": [ ["ECE 190", "CS 173"], 
+                 ["ECE 190", "MATH 213"],
+                 ["CS 125", "MATH 213"],
+                 ["CS 125", "CS 173"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "MATH 213",
+    "title": "Basic Discrete Mathematics",
+    "prereqs": [ ["MATH 220"], ["MATH 221"]],
+    "coreqs": [ [] ]
+  },
+  {
+    "name": "MATH 415",
+    "title": "Applied Linear Algebra",
+    "prereqs": [ ["MATH 241"]],
+    "coreqs": [ [] ]
   }
  ];
