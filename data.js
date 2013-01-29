cfd03397a38502958a01af72e4e76cf1167f0662
David Goldstein <dgoldstein0@gmail.com>
2013-01-28 20:06:16 -0600

    added through ece 350

--- data.js
+++ data.js
@@ -96,5 +96,88 @@ var json = [
     "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+340+-+Solid+State+Electronic+Devices",
     "prereqs": [["PHYS 214"]],
     "coreqs": [["ECE 329"]]
+  },
+  {
+    "name": "RHET 105",
+    "title": "Principles of Composition",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/RHET+105+-+Principles+of+Composition",
+    "prereqs": [[]],
+    "coreqs": [[]]
+  },
+  {
+    "name": "ECE 304",
+    "title": "Photonic Devices",
+    "prereqs": [["PHYS 214"]],
+    "coreqs": [[]]
+  },
+  {
+    "name": "ECE 307",
+    "title": "Techniques for Engrg Decisions",
+    "prereqs": [["ECE 210"]],
+    "coreqs": [["ECE 313"]]
+  },
+  {
+    "name": "ECE 310",
+    "title": "Digital Signal Processing",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+310+-+Digital+Signal+Processing+I",
+    "prereqs": [["ECE 210"]],
+    "coreqs": [[]]
+  },
+  {
+    "name": "ECE 311",
+    "title": "Digital Signal Processing Lab",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+311+-+Digital+Signal+Processing+Lab",
+    "prereqs": [[]],
+    "coreqs": [["ECE 310"]]
+  },
+  {
+    "name": "ECE 313",
+    "title": "Probability with Engrg Applic",
+    "crosslist": ["MATH 362"],
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+313+-+Probability+with+Engineering+Applications",
+    "prereqs": [["ECE 210"]],
+    "coreqs": [[]]
+  },
+  {
+    "name": "ECE 316",
+    "title": "Ethics and Engineering",
+    "crosslist": ["PHIL 316"],
+    "prereqs": [["RHET 105"]],
+    "coreqs": [[]]
+  },
+  {
+    "name": "ECE 330",
+    "title": "Power Circuits and Electromechanics",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+330+-+Power+Circuits+and+Electromechanics",
+    "prereqs": [["ECE 210"]],
+    "coreqs": [[]]
+  },
+  {
+    "name": "ECE 333",
+    "title": "Green Electric Energy",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+333+-+Green+Electric+Energy",
+    "prereqs": [["ECE 210"]],
+    "coreqs": [[]]
+  },
+  {
+    "name": "ECE 342",
+    "title": "Electronic Circuits",
+    "link": "https://wiki.engr.illinois.edu/pages/viewpage.action?pageId=194289968",
+    "prereqs": [["ECE 210"]],
+    "coreqs": [[]]
+  },
+  {
+    "name": "ECE 343",
+    "title": "Electronic Circuits Lab",
+    "link": "https://wiki.engr.illinois.edu/pages/viewpage.action?pageId=194289968",
+    "prereqs": [[]],
+    "coreqs": [["ECE 342"]]
+  },
+  {
+    "name": "ECE 350",
+    "title": "Fields and Waves II",
+    "link": "https://wiki.engr.illinois.edu/display/HKNDEN/ECE+350+-+Fields+and+Waves+II",
+    "prereqs": [["ECE 329"]],
+    "coreqs": [[]]
   }
  ];
